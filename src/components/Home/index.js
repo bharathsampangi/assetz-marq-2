26a1b2ad66515c4e670a02aac5772e0bb85d7ad5
bharathsampangi <bharathsampangi95@gmail.com>
2022-08-29 11:16:08 +0530

    completed township and gallery

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,16 @@
 import React from "react"
 import Hero from "../Hero"
+import Township from "../Township"
+import Middle from "../Middle"
+import Gallery from "../Gallery"
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <Township />
+            <Middle />
+            <Gallery />
         </>
     )
 }
